18890e0aa2048a8dc31ccd77815dd4c8a2cfc93d
Mykyta Koltunov <47789215+Gerphelius@users.noreply.github.com>
2019-07-15 01:27:31 +0300

    Update reverseNumber.js

--- FL11_HW8/homework/js/reverseNumber.js
+++ FL11_HW8/homework/js/reverseNumber.js
@@ -2,11 +2,11 @@ function reverseNumber(num) {
     let reversedNum = 0;
     let lastNum = 0;
 
-    while ( num ) {
+    while (num) {
         lastNum = num % 10;
         reversedNum = reversedNum * 10 + lastNum;
 
-        if ( num < 0) {
+        if (num < 0) {
             num = Math.ceil(num / 10);
         } else {
             num = Math.floor(num / 10);
@@ -16,4 +16,4 @@ function reverseNumber(num) {
     return reversedNum;
 }
 
-console.log( reverseNumber(-625) );
+console.log(reverseNumber(-625));
