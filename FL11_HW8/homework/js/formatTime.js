392a1dd4421cff32dcf276860816baf3b0768c28
Mykyta Koltunov <47789215+Gerphelius@users.noreply.github.com>
2019-07-15 01:29:59 +0300

    Update formatTime.js

--- FL11_HW8/homework/js/formatTime.js
+++ FL11_HW8/homework/js/formatTime.js
@@ -1,9 +1,9 @@
-function formatTime( time ) {
-    let days = Math.floor( time / 1440 );
-    let hours = Math.floor( time / 60 - (days * 24) );
-    let minutes = Math.floor( time - ( days * 1440 + hours * 60 ) );
+function formatTime(time) {
+    let days = Math.floor(time / 1440);
+    let hours = Math.floor(time / 60 - (days * 24));
+    let minutes = Math.floor(time - (days * 1440 + hours * 60));
 
     return days + ' day(s) ' + hours + ' hour(s) ' + minutes + ' minute(s) ';
 }
 
-console.log( formatTime(3603) );
+console.log(formatTime(3603));
